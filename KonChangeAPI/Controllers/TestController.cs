c7e0a872eabb1b786957310b0bfd83326328617e
Atan Games <atangames3@gmail.com>
2024-08-02 16:54:32 +0200

    Using ASP.NET instead

---
+++ KonChangeAPI/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KonChangeAPI.Controllers
+{
+    [ApiController]
+    [Route("testRequest")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public TestController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetTestrequest")]
+        public IEnumerable<string> Get()
+        {
+            List<string> result = new List<string>();
+            result.Add("Hello world");
+            return result;
+        }
+    }
+}
