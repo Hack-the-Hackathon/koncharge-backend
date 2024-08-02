418524816182c9bc90c002366e4fdd59c83bf89a
Atan Games <atangames3@gmail.com>
2024-08-02 21:22:53 +0200

    Added auth

---
+++ KonChangeAPI/Data/AuthDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace KonChargeAPI.Data
+{
+    public class AuthDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthDbContext(DbContextOptions options) : base(options) { }
+    }
+}
