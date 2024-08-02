1f7cea761a0a21a2d893840be1b95a8f86d9d6d2
Atan Games <atangames3@gmail.com>
2024-08-02 21:23:38 +0200

    --

--- KonChangeAPI/Data/AuthDbContext.cs
+++ KonChangeAPI/Data/AuthDbContext.cs
@@ -4,6 +4,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace KonChargeAPI.Data
 {
+    /// <summary>
+    /// Contains the identity data for the auth
+    /// </summary>
     public class AuthDbContext : IdentityDbContext<IdentityUser>
     {
         public AuthDbContext(DbContextOptions options) : base(options) { }
