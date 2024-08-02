5ceeba4e0e37429d43c1ec195b7def2461257c12
Atan Games <atangames3@gmail.com>
2024-08-02 15:55:33 +0200

    Added solution

---
+++ KonCharge/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace KonCharge;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Read();
+    }
+}
